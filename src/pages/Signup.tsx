225ff3a5895c4d7ba10ed122cc96baefe7e54d16
Nishita-16 <nishuverma105514@gmail.com>
2025-10-02 18:03:57 +0530

    some modifications

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -233,7 +233,7 @@ export default function Signup() {
     setIsLoading(true);
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/signup", {
+      const res = await fetch("http://localhost:5000/api/users/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         //body: JSON.stringify({ name, email, password }),
